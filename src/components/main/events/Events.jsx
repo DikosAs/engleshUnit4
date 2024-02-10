b6e62908dd9e209c31feeaa9fa6112cb303f986d
DikosAs <uraevdmitrij031@gmail.com>
2024-02-10 14:58:39 +0400

    Delete console.logs

--- src/components/main/events/Events.jsx
+++ src/components/main/events/Events.jsx
@@ -3,12 +3,9 @@ import Event from "./event/Event.jsx";
 import "./events.css"
 
 export default function Events() {
-  console.log(events_list)
   const events_map = events_list.map(
     (event) => (<Event title={event.title} date={event.date} />)
   )
-
-  console.log(events_map)
 
   return (
     <main>
