54f1775fb3b58e5857d827a302bb0e7e660bfee0
Marsh Meg <uraevdmitrij031@gmail.com>
2024-02-21 15:14:52 +0400

    edite text and events

--- src/components/main/about/About.jsx
+++ src/components/main/about/About.jsx
@@ -11,23 +11,11 @@ export default function About() {
         <img alt="" title="" height="" src={"https://shkolazavyalovskaya-r18.gosweb.gosuslugi.ru/netcat_files/20/260/IMG_1506.JPG"} />
       </div>
       <div className="text">
-        <p>1975 The very first school in the village of Zavyalovo was founded in 1853.
-
-          52 years later, in the 1905-1906 school year, a parochial school was opened.
-
-          In 1917, the school was transformed into an elementary school, in 1930 into a junior school, and in 1939 into a low-grade school.
-
-          In 1991, in connection with the opening of agricultural classes in the parallels of grades 8-11 and on the basis of an agreement on Great Britain with the Izhevsk Agricultural Academy, the school was renamed “Zavyalovskaya Secondary School-College”.
-
+        <p>The very first school in the village of Zavyalovo was founded in 1853.
           Since November 17, 2000, the school has been assigned the status of “Zavyalovskaya secondary school with in-depth study of individual subjects.”
-
-          In 2011, the organizational and legal form of education was changed - a municipal budgetary educational institution.
-
           Principals of different years played a major role in the formation of the secondary school: Mikhail Vasilievich Baronov, Nina Fillipovna Bondaryuk, Nadezhda Ivanovna Makhneva, Galina Aleksandrovna Ershova, Yuri Nikolaevich Kuznetsov, Tatyana Vitalievna Koscheeva, Elvira Safiullovna Dudyreva.
-
           Since 2019, the school has been headed by Nadezhda Anatolyevna Mokrushina.
-
-          Currently, the school teaches 2095 textbooks in 79 classes - sets. Teaching staff of 117 teachers. 17 teachers have the highest qualification category, 61 have the first.</p>
+          Currently, the school teaches 2095 in 79 classes - sets. Teaching staff of 117 teachers.</p>
       </div>
     </main>
   )
